6b345d3e6534033889cb3dc2fa6917b2cab9d67d
Godofallbacon <drewnaugle@comcast.net>
2022-09-22 10:01:33 -0400

    finishing hw

---
+++ src/main/java/Account.java
@@ -0,0 +1,8 @@
+public class Account {
+    public static void main(String[] args) {
+        //calling the getter and setter
+        Balance check = new Balance();
+        check.setBalance(-10);
+        System.out.println(check.getBalance());
+    }
+}
---
+++ src/main/java/Balance.java
@@ -0,0 +1,19 @@
+public class Balance {
+        private int balance;
+
+        public Balance() {
+            this.balance = balance;
+        }
+        //getter
+        public int getBalance() {
+            return balance;
+        }
+        //setter
+        public void setBalance(int balance) {
+            if (balance > 0)
+                this.balance = balance;
+
+
+        }
+    }
+
